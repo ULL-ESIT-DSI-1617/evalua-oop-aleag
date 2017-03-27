dfa8d0439bbba066ecec16378cc11e70513aca96
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-03-27 11:11:43 +0100

    Superando hito 3: reescribir las clases en ecma6

--- rTextCell.js
+++ rTextCell.js
@@ -1,21 +1,24 @@
 var tc = require('./textcell.js');
 var TextCell = tc.TextCell;
 
-// RTextCell
-function RTextCell(text) {
-  TextCell.call(this, text);
+// RTextCell ECMA6
+
+class RTextCell extends TextCell {
+    constructor(text) {
+        super(text);
+    }
+    draw(width, height) {
+        var result = [];
+        for (var i = 0; i < height; i++) {
+            var line = this.text[i] || "";
+            result.push(" ".repeat(width - line.length) + line);
+        }
+        return result;
+    };
 }
-RTextCell.prototype = Object.create(TextCell.prototype);
-RTextCell.prototype.draw = function(width, height) {
-  var result = [];
-  for (var i = 0; i < height; i++) {
-    var line = this.text[i] || "";
-    result.push(" ".repeat(width - line.length) + line);
-  }
-  return result;
-};
-// End RTextCell
+
+//End RTextCell
 
 module.exports = {
-  RTextCell: RTextCell
+    RTextCell: RTextCell
 };
